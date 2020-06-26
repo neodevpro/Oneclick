351d66408498725c845a1b6ac0da71fb2a7af360
Fusion <leemenhin@gmail.com>
2020-06-26 23:51:57 +0800

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -13,6 +13,13 @@ getd=$link$d
 wget $getc
 unzip *.zip
 wget -N --no-check-certificate $getd && bash ./download.sh
+
+echo "Enter Model Region (Example:SM-N9500 CHC): "
+read model
+info=$(python3 main.py checkupdate $model)
+python3 main.py download $info $model base.enc4
+python3 main.py decrypt4 $info $model base.enc4 base.zip
+
 base=${model:3:5}
 gete=$link$base$f
 wget -N --no-check-certificate $gete && bash ./base.sh
