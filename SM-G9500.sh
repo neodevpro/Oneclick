a25a347e15936ea72875521f907ff1c30289744a
Fusion <leemenhin@gmail.com>
2020-07-08 18:58:38 +0800

    Update SM-G9500.sh

--- SM-G9500.sh
+++ SM-G9500.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-base=$($basefw.zip)
+base=$(cat firmware)
 
 unzip -o $base.zip AP*.tar.md5 
 
@@ -151,7 +151,7 @@ rm -rf system/preloadFotaOnly
 
 zip -r -y SM-G9500.zip META-INF rootzip system boot.img data
 
-sudo rm -rf data META-INF rootzip $base.zip boot.img system
+sudo rm -rf data META-INF rootzip boot.img system
 
 
 
