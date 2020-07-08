728bf8db1bc26d07aa11f3c69129c3168e366296
Fusion <leemenhin@gmail.com>
2020-07-08 18:27:12 +0800

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -3,23 +3,77 @@ echo "   Welcome to use THis Tool "
 echo ""
 echo "    Power by Neodev Team"
 
-link="https://raw.githubusercontent.com/neodevpro/Oneclick/master/"
-sc="samcatcher.zip"
-f=".sh"
-d="download.sh"
-getc=$link$sc
-getd=$link$d
+echo -n "Checking environment... "
+echo ""
+if cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/os-release | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/os-release | grep -Eqi "debian"; then
+    release="debian"
+else
+    echo "==============="
+    echo "Not supported"
+    echo "==============="
+    exit
+fi
+clear
 
-wget $getc
-unzip *.zip
-wget -N --no-check-certificate $getd && bash ./download.sh
+echo -n "Checking dependencies... "
+echo ""
 
-echo "Enter Model Region (Example:SM-N9500 CHC): "
+echo "Preparing proper environment.."
+sudo apt update
+for dependencies in build-essential libssl-dev libffi-dev python3-dev python3-pip 
+do
+echo ""
+sudo apt install $dependencies
+done
+clear
+
+echo "Preparing proper library.."
+for pip3 in setuptools wheel progress clint simple-crypt aes
+do
+echo ""
+pip3 install $pip3
+done
+clear
+
+echo -n "Checking done. "
+echo ""
+
+echo "Downloading Sam-get tool.."
+wget -N --no-check-certificate https://raw.githubusercontent.com/neodevpro/sam-get/master/sam-get.zip
+unzip sam-get.zip
+clear
+ 
+echo "Enter Model and Region (Example:SM-N9500 CHC): "
 read model
 info=$(python3 main.py checkupdate $model)
-python3 main.py download $info $model base.enc4
-python3 main.py decrypt4 $info $model base.enc4 base.zip
+name=${model:0:8}"_"${model:9:3}"_"${info:0:13}
+python3 main.py download $info $model $name.enc4
+python3 main.py decrypt4 $info $model $name.enc4 $name.zip
+rm -rf $name.enc4 main.py sam-get.zip samcatcher
 
-base=${model:3:5}
+echo "You have download the firmware successfully "
+echo ""
+echo "odin file name : $name.zip "
+
+link="https://raw.githubusercontent.com/neodevpro/Oneclick/master/"
+f=".sh"
+
+echo "Now Deploying firmware "
+echo ""
+base=${model:0:8}
 gete=$link$base$f
 wget -N --no-check-certificate $gete && bash $base$f
+
+echo "You have port the rom successfully "
+echo ""
+echo "file name : $base.zip "
