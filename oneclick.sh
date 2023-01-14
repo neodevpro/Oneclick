1261a0bdb5acb7d3ce222695171eea9872e6e6e8
Fusion <leemenhin@gmail.com>
2023-01-15 02:35:47 +0800

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -137,7 +137,7 @@ unzip -q 8sbasefix.zip
 
 rm -rf 8sbasefix.zip
 
-cp -arf 8sbasefix/system/. system/
+sudo cp -arf 8sbasefix/system/. system/
 
 rm -rf 8sbasefix
 
@@ -161,7 +161,7 @@ mkdir rootzip
 
 mkdir system/preload/Magisk
 path=`wget -qO- -t1 -T2 "https://api.github.com/repos/topjohnwu/Magisk/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
-wget -q -O system/preload/Magisk/Magisk.apk https://github.com/topjohnwu/Magisk/releases/download/$path/Magisk-$path.apk
+sudo wget -q -O system/preload/Magisk/Magisk.apk https://github.com/topjohnwu/Magisk/releases/download/$path/Magisk-$path.apk
 
 echo "Downloding ${model:0:8} Kernel ... "
 echo ""
@@ -190,7 +190,7 @@ sudo sed -i "s/ro.config.kap=true/ro.config.kap=false/g" system/build.prop
 
 wget -q https://raw.githubusercontent.com/neodevpro/resources/master/add_to_buildprop.sh
 
-bash ./add_to_buildprop.sh
+sudo bash ./add_to_buildprop.sh
 
 wget -q https://raw.githubusercontent.com/neodevpro/resources/master/csc_tweaks.sh
 
@@ -198,166 +198,9 @@ sh ./csc_tweaks.sh
 
 rm -rf csc_tweaks.sh add_to_buildprop.sh
 
-rm -rf system/recovery-from-boot.p
-rm -rf system/app/BBCAgent
-rm -rf system/app/KnoxAttestationAgent
-rm -rf system/app/MDMApp
-rm -rf system/app/SecurityLogAgent
-rm -rf system/app/SecurityProviderSEC
-rm -rf system/app/UniversalMDMClient
-rm -rf system/priv-app/ContainerAgent*
-rm -rf system/container
-rm -rf system/etc/permissions/knoxsdk_edm.xml
-rm -rf system/etc/permissions/knoxsdk_mdm.xml
-rm -rf system/etc/recovery-resource.dat
-rm -rf system/priv-app/DiagMonAgent
-rm -rf system/priv-app/KLMSAgent
-rm -rf system/priv-app/KnoxCore
-rm -rf system/priv-app/knoxvpnproxyhandler
-rm -rf system/priv-app/Rlc
-rm -rf system/priv-app/SamsungPayStub
-rm -rf system/priv-app/SecureFolder
-rm -rf system/priv-app/SPDClient
-rm -rf system/priv-app/TeeService
-rm -rf system/lib/libvkservice
-rm -rf system/lib64/libvkservice
-rm -rf system/lib/libvkjni
-rm -rf system/lib64/libvkjni
-rm -rf system/etc/init/bootchecker.rc
-rm -rf system/secure_storage_daemon_system.rc
-rm -rf system/lib/liboemcrypto
-
-
-
-
-rm -rf system/preload/Excel_SamsungStub
-rm -rf system/preload/Gear360Editor_Beyond
-rm -rf system/preload/LinkedIn_SamsungStub
-rm -rf system/preload/PowerPoint_SamsungStub
-rm -rf system/preload/SamsungVideo
-rm -rf system/preload/SAssistant_downloadable
-rm -rf system/preload/Word_SamsungStub
-rm -rf system/preload/StoryEditor_Dream_N
-rm -rf system/preload/SlowMotionVideoEditor
-rm -rf system/preload/SmartSwitch
-rm -rf system/preload/SmartSwitchAgent
-rm -rf system/preload/CocktailQuickTool
-rm -rf system/preload/SamsungMagnifier3
-rm -rf system/preload/SearchWidgetAPP
-rm -rf system/preload/SPdfNote
-rm -rf system/preload/VideoTrimmer
-rm -rf system/preload/VideoEditorLite_Dream_N
-rm -rf system/preload/NaverV_N
-rm -rf system/preload/HongbaoAssistant
-rm -rf system/preload/MateAgent
-rm -rf system/preload/LedCoverAppDream
-rm -rf system/preload/SamsungCloudClient
-rm -rf system/preload/SecEmail_P
-rm -rf system/preload/VisionIntelligence2_stub
-rm -rf system/preload/WechatPluginMiniApp
-rm -rf system/preloadFotaOnly
-rm -rf system/preload/LinkSharing*
-rm -rf system/app/ChinaUnionPay
-rm -rf system/app/PDFViewer
-rm -rf system/app/ClipboardEdge
-rm -rf system/app/CocktailQuickTool
-rm -rf system/app/Facebook_stub
-rm -rf system/app/GearManagerStub
-rm -rf system/app/MSSkype_stub
-rm -rf system/app/PlayAutoInstallConfig
-rm -rf system/app/SBrowserEdge
-rm -rf system/app/SlowMotionVideoEditor
-rm -rf system/app/StoryEditor_Dream_N
-rm -rf system/app/VideoTrimmer
-rm -rf system/app/VisionIntelligence2_stub
-rm -rf system/app/WebManual
-rm -rf system/app/Yahoo*
-rm -rf system/app/YouTube
-rm -rf system/app/CarmodeStub
-rm -rf system/app/EasterEgg
-rm -rf system/app/EasymodeContactsWidget81
-rm -rf system/app/FBAppManager_NS
-rm -rf system/app/FlipboardBriefing
-rm -rf system/app/InteractivePanoramaViewer_WQHD
-rm -rf system/app/KidsHome_Installer
-rm -rf system/app/Maps
-rm -rf system/app/MirrorLink
-rm -rf system/app/Panorama360Viewer
-rm -rf system/app/SmartSwitchAgent
-rm -rf system/app/SmartReminder
-rm -rf system/app/PreloadAppDownload
-rm -rf system/app/Gmail2
-rm -rf system/app/MotionPanoramaViewer
-rm -rf system/app/SelfMotionPanoramaViewer
-rm -rf system/app/WebViewStub
-rm -rf system/app/VideoEditorLite_Dream_N
-rm -rf system/app/Kaiti
-rm -rf system/app/Miao
-rm -rf system/app/ShaoNv
-rm -rf system/app/EasyOneHand3
-rm -rf system/app/ARCore
-rm -rf system/app/GoogleVrServices
-rm -rf system/app/SmartMirroring
-rm -rf system/app/LinkSharing*
-rm -rf system/priv-app/LinkSharing*
-rm -rf system/priv-app/HybridRadio_P
-rm -rf system/priv-app/AlipayService
-rm -rf system/priv-app/AppsEdgePanel_v3.2
-rm -rf system/priv-app/Excel_SamsungStub
-rm -rf system/priv-app/FBInstaller_NS
-rm -rf system/priv-app/FBServices
-rm -rf system/priv-app/Finder
-rm -rf system/priv-app/FotaAgent
-rm -rf system/priv-app/GalaxyAppsWidget_Phone_Dream
-rm -rf system/priv-app/Gear360Editor_Beyond
-rm -rf system/priv-app/OneDrive_Samsung_v3
-rm -rf system/priv-app/PowerPoint_SamsungStub
-rm -rf system/priv-app/TaskEdgePanel_v3.2
-rm -rf system/priv-app/Velvet
-rm -rf system/priv-app/Word_SamsungStub
-rm -rf system/priv-app/GearVRService
-rm -rf system/priv-app/GoogleDaydreamCustomization
-rm -rf system/priv-app/LinkedIn_SamsungStub
-rm -rf system/priv-app/BixbyHome
-rm -rf system/priv-app/HotwordEnrollment*
-rm -rf system/priv-app/AuthFramework
-rm -rf system/priv-app/BeaconManager
-rm -rf system/priv-app/Bixby
-rm -rf system/priv-app/BixbyAgentStub
-rm -rf system/priv-app/BixbyService
-rm -rf system/priv-app/BixbyWakeup
-rm -rf system/priv-app/EasySetup
-rm -rf system/priv-app/knoxanalyticsagent
-rm -rf system/priv-app/LedCoverAppDream
-rm -rf system/priv-app/LedCoverService
-rm -rf system/priv-app/ManagedProvisioning
-rm -rf system/priv-app/MateAgent
-rm -rf system/priv-app/PaymentFramework
-rm -rf system/priv-app/PeopleStripe
-rm -rf system/priv-app/SamsungCloudClient
-rm -rf system/priv-app/SamsungBilling
-rm -rf system/priv-app/SamsungMagnifier3
-rm -rf system/priv-app/SendHelpMessage
-rm -rf system/priv-app/SVoicePLM
-rm -rf system/priv-app/SettingsBixby
-rm -rf system/priv-app/SystemUIBixby2
-rm -rf system/priv-app/CocktailBarService_v3.2
-rm -rf system/priv-app/SetupWizard
-rm -rf system/priv-app/GoogleRestore
-rm -rf system/priv-app/BixbyHome_Disable
-rm -rf system/priv-app/DynamicLockscreen
-rm -rf system/priv-app/SamsungCloudEnabler
-rm -rf system/priv-app/SmartEpdgTestApp
-rm -rf system/priv-app/SVoiceIME
-
-rm -rf system/priv-app/StickerCenter
-rm -rf system/priv-app/StickerFaceAR
-rm -rf system/priv-app/StickerWatermark
-
-
 echo "Packing the Rom ... "
 echo ""
-zip -r -q -y StockMod.zip META-INF system rootzip boot.img
+sudo zip -r -q -y StockMod.zip META-INF system rootzip boot.img
 
 rm -rf META-INF system rootzip boot.img 
 
