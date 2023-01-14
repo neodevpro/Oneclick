46f6762fb252e8ac57b726c484b3ed7d9ee36e56
Fusion <leemenhin@gmail.com>
2023-01-15 02:43:12 +0800

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -159,7 +159,7 @@ echo ""
 
 mkdir rootzip
 
-mkdir system/preload/Magisk
+sudo mkdir system/preload/Magisk
 path=`wget -qO- -t1 -T2 "https://api.github.com/repos/topjohnwu/Magisk/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 sudo wget -q -O system/preload/Magisk/Magisk.apk https://github.com/topjohnwu/Magisk/releases/download/$path/Magisk-$path.apk
 
